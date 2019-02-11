6d28ac1fdebcae4e5d8ed5cda521f66a54cba063
Teodor Moroz <teomoroz@gmail.com>
2019-02-11 08:55:45 +0200

    Added exploration of age and breed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 import json
 import os
 
@@ -9,6 +12,7 @@ pd.set_option('display.width', 1000)
 train_data = pd.read_csv('../data/all/train/train.csv')
 
 train_sen = os.listdir('../data/all/train_sentiment/')
+train_metadata = os.listdir('../data/all/train_metadata')
 
 print('Data HEAD: ')
 print(train_data.head())
@@ -40,3 +44,37 @@ def get_sentiment(df, sen_source, test_train, key):
 
 train_data['sent_score'] = get_sentiment(train_data, train_sen, 'train', 'score')
 train_data['sent_magnitude'] = get_sentiment(train_data, train_sen, 'train', 'magnitude')
+
+# train_data['sent_score'].plot.hist()
+# plt.show()
+#
+# train_data['sent_magnitude'].plot.hist()
+# plt.show()
+
+# train_data['PhotoAmt'].value_counts().plot.bar()
+# plt.show()
+#print(train_data.corr())
+#
+# train_data["AdoptionSpeed"].value_counts().sort_index().plot('barh')
+# plt.show();
+#
+# train_data["Type"].value_counts().sort_index().plot('barh')
+# plt.show();
+
+# train_data["Age"].plot(kind='hist')
+# plt.show()
+#
+
+train_data['Type'] = train_data['Type'].apply(lambda x: 'Dog' if x == 1 else 'Cat')
+
+# sns.barplot(x='AdoptionSpeed', y='Age', hue='Type', data=train_data)
+# plt.show()
+
+
+train_data['IsMixed'] = train_data['Breed2'].apply(lambda x: 0 if x == 0 else 1)
+print(train_data['IsMixed'].unique())
+
+#train_data['CatIsMixed'] = train_data
+
+sns.barplot(x='AdoptionSpeed', y='IsMixed', data=train_data)
+plt.show()
